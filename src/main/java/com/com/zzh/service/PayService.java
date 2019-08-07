b0851ab234f54c43ba950de61002896b1efec865
zzh <qq1663867295@outlook.com>
2019-08-07 22:48:25 +0800

    retty

---
+++ src/main/java/com/com/zzh/service/PayService.java
@@ -0,0 +1,28 @@
+package com.com.zzh.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalTime;
+
+@Service
+public class PayService {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final int totalNum = 100000;
+
+
+    @Retryable(value = Exception.class,maxAttempts = 3,backoff = @Backoff(delay = 2000,multiplier = 1.5))
+    public int minGoodsnum(int num) throws Exception{
+        logger.info("minGoodsnum开始"+ LocalTime.now());
+        if(num <= 0){
+            throw new Exception("数量不对");
+        }
+        logger.info("minGoodsnum执行结束");
+        return totalNum - num;
+    }
+}
